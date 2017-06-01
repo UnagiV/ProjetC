0671b748ef446fd4bce0c4cc2552ef2f1489caf1
Heol <mathieujouvet@MacBook-Pro-de-Mathieu.local>
2017-06-01 10:13:30 +0200

    fourmi

---
+++ app/Collaborateur.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
+
+class Collaborateur extends Model
+{
+
+    protected $primaryKey = 'Id_TCa';
+
+    protected $table = 'T_Collaborateurs';
+
+    protected $fillable = ['Collaborateur_TCa', 'Date_In_TCa','Date_Out_TCa', 'Acces_TCa','Responsable_TCa','TR_TCa'];
+
+    public $timestamps = false;
+
+    protected $dateFormat = "d-m-Y";
+
+    // public $dates = Carbon::createFromFormat('d-m-Y','31-05-2017');
+
+
+
+}
