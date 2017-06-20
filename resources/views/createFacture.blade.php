05dead12c3919d61ce6cc4db41c008adeedeafce
Heol <jouvetmathieu@free.fr>
2017-06-20 17:36:41 +0200

    maj Mardi 20

--- resources/views/createFacture.blade.php
+++ resources/views/createFacture.blade.php
@@ -7,8 +7,18 @@
 			<div class="panel-heading">Création d'une facture</div>
 			<div class="panel-body">
 				<div class="col-sm-12">
-					{!! Form::open(['route' => 'collaborateur.store', 'class' => 'form-horizontal panel']) !!}
-					<div class="form-group {!! $errors->has('Collaborateur_TCa') ? 'has-error' : '' !!}">
+					{!! Form::open(['route' => 'facture.store', 'class' => 'form-horizontal panel']) !!}
+
+					<div class="form-group {!! $errors->has('Ste_TCl') ? 'has-error' : '' !!}">Nom du Client
+              <select class="form-control" name="Ste_TCl">
+								<option selected="selected"  hidden="hidden" >Nom du Client</option>
+                @for($i = 0; $i < $entreprise->count(); $i++)
+                <option value="{{$entreprise[$i]}}">{{$entreprise[$i]}}</option>
+                @endfor
+							</select>
+          </div>
+
+					<!-- <div class="form-group {!! $errors->has('Collaborateur_TCa') ? 'has-error' : '' !!}">
 						{!! Form::text('Collaborateur_TCa', null, ['class' => 'form-control', 'placeholder' => 'Nom']) !!}
 						{!! $errors->first('Collaborateur_TCa', '<small class="help-block">:message</small>') !!}
 					</div>
@@ -24,22 +34,17 @@
           <div class="form-group">
 					  	{!! Form::text('Acces_TCa', null, ['class' => 'form-control', 'placeholder' => 'Accès']) !!}
 
-					</div>
-          <!-- <div class="form-group"> -->
-					  	<!-- {!! Form::text('Responsable_TCa', null, ['class' => 'form-control', 'placeholder' => 'Responsable']) !!} -->
-					<!-- </div> -->
-					<div class="form-group  {!! $errors->has('Responsable_TCa') ? 'has-error' : '' !!}">
-					  	<!-- {!! Form::select('Responsable_TCa',['Cédric Dupuis', 'Bastien Jambon'], null, ['class' => 'form-control', 'placeholder' => 'Responsable']) !!} -->
-
-							<select class="form-control" name="Responsable_TCa">
-								<option selected="selected" disabled="disabled" hidden="hidden" value>Responsable</option>
-								<option value="Cédric Dupuis">Cédric Dupuis</option>
-								<option value="Bastien Jambon">Bastien Jambon</option>
+					</div> -->
+					<div class="form-group {!! $errors->has('Responsable_TCa') ? 'has-error' : '' !!}">Responsable
+              <select class="form-control" name="Responsable_TCa">
+								<option selected="selected"  hidden="hidden" value ="" >Responsable</option>
+                @for ($i = 0; $i < $responsables->count(); $i++)
+								<option value="{{$responsables[$i]->Collaborateur_TCa}}">{{$responsables[$i]->Collaborateur_TCa}}</option>
+                @endfor
 							</select>
-							{!! $errors->first('Responsable_TCa', '<small class="help-block">:message</small>') !!}
+          </div>
 
 
-					</div>
 
 
 					{!! Form::submit('Envoyer', ['class' => 'btn btn-primary pull-right']) !!}
